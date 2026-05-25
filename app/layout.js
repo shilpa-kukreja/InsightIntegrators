256905c6c2465f847378f11de2b311865de61071
shilpa-kukreja <recreaters.services@gmail.com>
2026-05-25 15:52:33 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -181,6 +181,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-5JKDPWX5');</script>
 <!-- End Google Tag Manager -->
+                <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JKDPWX5"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
         {/* Facebook Pixel */}
         <Script id="facebook-pixel" strategy="afterInteractive">
