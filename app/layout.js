4dcb1db8095c5ebaa6584b233a028e7f3acbac6e
shilpa-kukreja <recreaters.services@gmail.com>
2026-05-25 15:50:11 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -165,15 +165,22 @@ export default function RootLayout({ children }) {
           strategy="afterInteractive"
         />
 
-        <Script id="google-ads" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+        // <Script id="google-ads" strategy="afterInteractive">
+        //   {`
+        //     window.dataLayer = window.dataLayer || [];
+        //     function gtag(){dataLayer.push(arguments);}
+        //     gtag('js', new Date());
 
-            gtag('config', 'AW-18100457132');
-          `}
-        </Script>
+        //     gtag('config', 'AW-18100457132');
+        //   `}
+        // </Script>
+                <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5JKDPWX5');</script>
+<!-- End Google Tag Manager -->
 
         {/* Facebook Pixel */}
         <Script id="facebook-pixel" strategy="afterInteractive">
